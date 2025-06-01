a6196821c78c8bf4ed6be87bfba1ccaac5d5584b
Miguelito2926 <juniorprol@hotmail.com>
2025-06-01 15:14:29 -0300

    feat:criando relacionamentos muitos para muitos em Product e Category, configurando coluna para receber textos longos e colinm unique para o campo email

--- src/main/java/com/ednaldo/edcommerce/entities/Product.java
+++ src/main/java/com/ednaldo/edcommerce/entities/Product.java
@@ -2,7 +2,9 @@ package com.ednaldo.edcommerce.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_product")
@@ -11,10 +13,20 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
+
     private Double price;
     private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+    joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
     public Product() {
     }
@@ -65,6 +77,10 @@ public class Product {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
