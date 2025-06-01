68fe3ea3c676fd28b3ccdb8c1b50c3f6379241d0
Miguelito2926 <juniorprol@hotmail.com>
2025-06-01 11:16:07 -0300

    criando projeto

---
+++ src/main/java/com/ednaldo/edcommerce/EdcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ednaldo.edcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ednaldo/edcommerce/EdcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednaldo.edcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
