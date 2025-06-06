a0cc3389301298ff783f64043511a801fc9e0d66
Miguelito2926 <juniorprol@hotmail.com>
2025-06-06 18:55:10 -0300

    feat: otimizando o DeleteProduct

--- src/main/java/com/ednaldo/edcommerce/services/ProductService.java
+++ src/main/java/com/ednaldo/edcommerce/services/ProductService.java
@@ -3,12 +3,11 @@ package com.ednaldo.edcommerce.services;
 import com.ednaldo.edcommerce.dto.ProductDTO;
 import com.ednaldo.edcommerce.entities.Product;
 import com.ednaldo.edcommerce.repositories.ProductRepository;
+import com.ednaldo.edcommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ProductService {
@@ -24,9 +23,9 @@ public class ProductService {
         return all.map(ProductDTO::new);
     }
 
-    public ProductDTO getProductById(Long id) throws Exception {
+    public ProductDTO getProductById(Long id) throws ResourceNotFoundException {
         var product = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResourceNotFoundException("Resource Not Found Id: " + id));
 
         return new ProductDTO(product);
     }
@@ -41,7 +40,7 @@ public class ProductService {
     public ProductDTO updateProduct(ProductDTO productDTO, Long id) {
 
         Product prod = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResourceNotFoundException("Resource Not Found Id: " + id));
 
         prod.setName(productDTO.getName());
         prod.setDescription(productDTO.getDescription());
@@ -53,10 +52,11 @@ public class ProductService {
         return productDTO;
     }
 
-    public void deleteProduct(Long id) {
-        Product prod = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        
-        productRepository.delete(prod);
+    public void deleteProduct(Long id) throws ResourceNotFoundException {
+        if (!productRepository.existsById(id)) {
+            throw  new ResourceNotFoundException("Resource Not Found Id: " + id);
+        }
+
+        productRepository.deleteById(id);
     }
 }
