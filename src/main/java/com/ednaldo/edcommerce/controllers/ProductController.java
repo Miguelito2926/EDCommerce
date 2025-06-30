a9fb9e00835972346f08a26b4d88a51aa32124a1
Miguelito2926 <juniorprol@hotmail.com>
2025-06-30 19:43:56 -0300

    feat: Get User logged

--- src/main/java/com/ednaldo/edcommerce/controllers/ProductController.java
+++ src/main/java/com/ednaldo/edcommerce/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,6 +31,7 @@ public class ProductController {
 //        return ResponseEntity.created(uri).body(productDTO);
 //    }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insertProduct(@Valid @RequestBody ProductDTO request) {
         ProductDTO productDTO = productService.insertProduct(request);
@@ -48,11 +50,13 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO, @PathVariable Long id) throws Exception {
         return ResponseEntity.ok(productService.updateProduct(productDTO, id));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) throws ResourceNotFoundException {
         productService.deleteProduct(id);
--- src/main/java/com/ednaldo/edcommerce/services/UserService.java
+++ src/main/java/com/ednaldo/edcommerce/services/UserService.java
@@ -1,12 +1,18 @@
-package com.ednaldo.edcommerce.repositories;
+package com.ednaldo.edcommerce.services;
 
+import com.ednaldo.edcommerce.dto.UserDTO;
 import com.ednaldo.edcommerce.entities.Role;
 import com.ednaldo.edcommerce.entities.User;
 import com.ednaldo.edcommerce.projections.UserDetailsProjection;
+import com.ednaldo.edcommerce.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,5 +38,23 @@ public class UserService implements UserDetailsService {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return userRepository.findByEmail(username).get();
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    @Transactional
+    public UserDTO getMe() {
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
